f31f446741da9670742878a3d7f6e4102b25c54b
Abinaya Sarva <abinaya@fb.com>
2014-10-08 08:20:56 -0700

    Initial Commit

---
+++ tipCalculator/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  tipCalculator
+//
+//  Created by Abinaya Sarva on 10/8/14.
+//  Copyright (c) 2014 Sarva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipCalculator
+//
+//  Created by Abinaya Sarva on 10/8/14.
+//  Copyright (c) 2014 Sarva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}
